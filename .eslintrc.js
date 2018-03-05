aa64258e3a80beb99d64cea7e5744f604a9c491a
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-03-05 23:39:43 +0000

    resolved Added yarn add eslint eslint-plugins lint-staged etc
    .editorconfig file added
    yarn run adddev eslint
    yarn run adddev eslint-plugin-import eslint-plugin-objects eslint-plugin-filenames
    yarn run adddev eslint-plugin-react eslint-plugin-jsx-a11y eslint-plugin-jest
    yarn run adddev lint-staged
    eslint --init to create an initial .eslintrc.js config file with
    node+browser, ES6/JSX, tabs, unix line ends, single quotes, no semicolons
    added eslint config for objects, filenames, react and jest plugins
    configured lint-staged as a husky precommit action

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+/* eslint-disable filenames/match-regex */
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true,
+		'jest/globals': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:react/all', // or recommended
+		'plugin:jsx-a11y/strict', // or recommended
+		'plugin:jest/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'experimentalObjectRestSpread': true,
+			'jsx': true
+		},
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'objects',
+		'filenames',
+		'react',
+		'jsx-a11y',
+		'jest'
+	],
+	'rules': {
+		// style - general settins from eslint --init
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		// style - eslint-plugin-object configuration
+		'objects/no-object-properties-first-line': [ 'error' ],
+		'objects/no-object-properties-last-line': [ 'error' ],
+		'objects/no-object-properties-one-line': [ 'error' ],
+		'objects/no-object-property-split': [ 'error' ],
+		// eslint-plugin-filenames configuration
+		'filenames/no-index': [ 'error' ],
+		'filenames/match-regex': [ 'error', '^[a-zA-Z-]+$' ],
+		'filenames/match-exported': [ 'error', [ null, 'kebab', 'camel' ] ]
+	}
+}
